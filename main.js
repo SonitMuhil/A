35dee30e3b7668db2ef06e082432bf2fd908b241
SonitMuhil <77870888+SonitMuhil@users.noreply.github.com>
2021-12-11 11:07:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Intialized');
+}
+
+function gotPoses(results)
+{
+   if(results.length > 0)
+   {
+       console.log(results);
+       console.log("philtrum x = " + results[0].pose.philtrum.x);
+       console.log("philtrum y = " + results[0].pose.philtrum.y);
+       console.log("lips x = " + results[1].pose.lips.x);
+       console.log("lips y = " + results[1].pose.lips.y);
+   } 
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
